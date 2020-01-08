e6d6e16d1e7f836c90bf6e0496cbd906b7ccc59a
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-01-08 14:14:19 -0500

    added a few examples of php variables

---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Variables in PHP</title>
+</head>
+<body>
+    <?php
+    $name = "Daniel Ducas-Viramontes";
+    $age = "29";
+    
+    echo $name;
+    
+    ?>
+</body>
+</html>
