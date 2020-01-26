b8e24ede250cfa7582d72632adba52fe4f391701
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-01-26 15:36:20 -0500

    finished foreach.php

--- foreach.php
+++ foreach.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>For Each</title>
 </head>
 <body>
+    <h1>For Each</h1>
+
+    <?php
     
+    $numbers = [245,123,141,5,6,677,232];
+    foreach($numbers as $number){
+        echo $number . "<br>";
+    }
+    ?>
 </body>
 </html>
