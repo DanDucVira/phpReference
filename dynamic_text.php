55d7d19f26f92620775b855053e7a04f2d26431d
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-01-25 23:07:34 -0500

    added a few examples

---
+++ dynamic_text.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <title>Dynamic Data</title>
+</head>
+<body>
+<?php
+//This is a comment in php
+$title = "My Website";
+
+?>
+
+    <h1><?php echo $title; ?></h1>
+</body>
+</html>
