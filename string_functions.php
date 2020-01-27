f816a08e82ec4b967094a8b97041d0ae7ad08f1b
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-01-27 13:05:53 -0500

    finished string_functions.php

---
+++ string_functions.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>string functions</title>
+</head>
+<body>
+    <h1>String functions</h1>
+
+    <?php
+        $strings = "hello student";
+        print($strings);
+        echo "<br>";
+        echo strlen($strings);
+
+        echo "<br>";
+
+        echo strtoupper($strings);
+
+        
+        echo "<br>";
+
+        echo strtolower($strings);
+
+
+    
+    
+    ?>
+</body>
+</html>
