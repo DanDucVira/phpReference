05e4b68da6d8a3b03c4f9eabaed22145765bfa4d
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-01-27 13:49:43 -0500

    validating the form values

--- form.php
+++ form.php
@@ -2,15 +2,30 @@
     
 
     if(isset($_POST['submit'])){
+        $name = ["Daniel", "Student", "peter", "samid","tom"];
+        $minimun = 5;
+        $maximun = 10;
        $userName = $_POST['username'];
         $passWord = $_POST['password'];
 
-        echo "Hello," . $userName;
-        echo "<br>";
-        echo "Your password is " . $passWord;
-    }
+
+        if(strlen($username < $minimun)){
+            echo "username has to be longer than five";
+        }
+        
+        if(strlen($username > $maximun)){
+            echo "username has to be shorter than 10";
+        }
+
+        if(!in_array($username,$name)){
+                echo "Sorry you cannot login";
+        }
+    //     echo "Hello," . $userName;
+    //     echo "<br>";
+    //     echo "Your password is " . $passWord;
+    // }
     
-    
+    } 
     
     ?>
 
