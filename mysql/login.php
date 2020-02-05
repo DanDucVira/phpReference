aa905cb68d679f7382561d730789dcc2a8a73af1
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-02-05 13:22:00 -0500

    receiving post data from check

--- mysql/login.php
+++ mysql/login.php
@@ -1,3 +1,18 @@
+<?php
+if (isset($_POST['submit'])) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    echo $username;
+    echo "<br>";
+    echo $password;
+
+}
+
+?>
+
+
 <!doctype html>
 <html lang="en">
   <head>
