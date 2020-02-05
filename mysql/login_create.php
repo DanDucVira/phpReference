4b8afe3141705b437d92ed6f1709fa96fcc10ab2
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2020-02-05 13:58:24 -0500

    creating records into the database table with php

--- mysql/login_create.php
+++ mysql/login_create.php
@@ -1,6 +1,8 @@
 <?php
+
 $username = $_POST["username"];
 $password = $_POST["password"];
+
 if (isset($_POST['submit'])) {
 
     //conntecting to the database
@@ -12,14 +14,15 @@ if (isset($_POST['submit'])) {
         die("Database connection failed");
     }
 
-    // if ($username && $password) {
-    //     echo $username;
-    //     echo "<br>";
-    //     echo $password;
-    // } else {
-    //     echo "this field cannot be blank";
-    // }
+    $query = "INSERT INTO users(username,password) ";
+    $query .= "VALUES ('$username', '$password')";
 
+    $result = mysqli_query($connection, $query);
+
+    if (!$result) {
+        die("Query FAILED " . mysqli_error());
+
+    }
 }
 
 ?>
@@ -40,7 +43,7 @@ if (isset($_POST['submit'])) {
   <body>
 <div class="container">
     <div class="col-sm-6">
-        <form action="login.php" method="post">
+        <form action="login_create.php" method="post">
         <div class="form-group">
         <label for="username">Username</label>
         <input type="text" name="username" class="form-control">
